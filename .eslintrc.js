88690976aef0475713e906f4c4331ad65c48cb88
Anna Saushkina <saushkinanna@gmail.com>
2023-11-29 01:23:34 +0300

    fix: eslint WORKING

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,43 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: [
-        "airbnb-typescript",
-        "standard-with-typescript",
-        "plugin:@typescript-eslint/recommended", 
-        "plugin:prettier/recommended",
-        "prettier",
-    ],
-    parserOptions: {
-        ecmaVersion: "latest", 
-        sourceType: "module" 
-    }
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    // 'no-unused-vars': 0,
+    // 'no-restricted-imports': 0,
+    // '@typescript-eslint/no-unused-vars': 2,
+    // 'react-refresh/only-export-components': [1, { allowConstantExport: true }],
+    // 'react/function-component-definition': [
+    //   2,
+    //   { namedComponents: 'arrow-function' },
+    // ],
+    // 'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
+    // 'react/require-default-props': 0,
+    // 'import/prefer-default-export': 0,
+    // 'import/extensions': 0,
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import './App.css';
+import filter from './components/filter';
+import paintings from './components/paintings';
+import pagination from './components/pagination';
+// import theme from './modules/App.module.scss';
 
 function App() {
+
+
   return (
-    <div className="App">
-     Hello fwx
+    <div className={theme.dark_theme}>
+
+      <header className='header'>
+        <img className='header__logo' src="./" alt="logo" />
+        <button className='header__theme-switch'></button>
+      </header>
+     
+     <filter/>
+      {/* <paintings/>
+      <pagination/> */}
+
     </div>
   );
-}
 
+}
+var 12
 
 export default App;
